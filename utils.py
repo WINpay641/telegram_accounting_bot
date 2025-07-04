fd48e0e7f557ff7d131441c98b2fcc73ee1380ec
telegram_accounting_bot <taiyang25001@gmail.com>
2025-07-04 18:20:34 +0700

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def parse_amount(text):
-    try:
-        match = re.search(r"[-+]?\d+(\.\d+)?", text)
-        return float(match.group()) if match else None
-    except:
-        return None
